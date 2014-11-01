588268da11117dd3d491791c71d6b0c04e069bfa
Winston Sumalia <wasumalia@gmail.com>
2014-11-01 20:41:36 +0800

    validation ok for state and outcome

--- best.R
+++ best.R
@@ -5,22 +5,35 @@ best <- function(state,outcome) {
   ## Read outcome data
   outcomeOfCare <- read.csv("rprog-data-ProgAssignment3-data/outcome-of-care-measures.csv", colClasses = "character")
   UniqState <- unique(outcomeOfCare$State)
-  UniqHosp <- unique(outcomeOfCare$Hospital.name)
+  ##UniqHosp <- unique(outcomeOfCare$Hospital.name)
+  UniqOutcome <- c("heart attack","heart failure","pneumonia")
+  
   
   ## Check that state and outcome are valid
-  for (i in 1:length(UniqState))
-    print (c(i,UniqState[i]))
-    if (state == UniqState[i] ) print(c("State matched!!", i))
-      else if (i == length(UniqState)) stop("State is not Recognized") 
-        ##if (state != UniqState[i] ) {print(c(UniqState[i],i))
-      ##                                 }
-    
-    
+  CheckState<- function(state){
+                for (i in 1:length(UniqState))
+                  ##print (c(i,UniqState[i]))
+                  if (state == UniqState[i] ) { print(c("State matched!!", i))
+                                                return (state)}
+                    else if (i == length(UniqState)) stop("State is not Recognized")
+                }
+  
+  CheckOutcome<- function(outcome){
+    for (i in 1:length(UniqOutcome))
+      if (outcome == UniqOutcome[i] ) { print(c("outcome matched!!", i))
+                                    return (outcome)}
+    else if (i == length(UniqOutcome)) stop("Outcome is not Recognized")
+  }
+   
+  state<-CheckState(state)
+  outcome<-CheckOutcome(outcome)
+ 
+  
   ##if (state != )
   
   ## Return hospital name in that state with the lowest 30-day death rate
-  if (outcome == "heart attack") minrate <- min(outcomeOfCare[,11]) 
-  else if (outcome == "heart failure") minrate <- min(outcomeOfCare[,17])
-  ##else 
+  ##if (outcome == "heart attack") minrate <- min(outcomeOfCare[,11]) 
+  ##else if (outcome == "heart failure") minrate <- min(outcomeOfCare[,17])
+ 
   
 }

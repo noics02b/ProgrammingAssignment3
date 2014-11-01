389bf903c8eb8bf2f6da07586ec79ea59cd900e5
Winston Sumalia <wasumalia@gmail.com>
2014-11-02 02:18:43 +0800

    initial file -- blank

---
+++ rankhospital.R
@@ -0,0 +1,8 @@
+rankhospital <- function (state, outcome, num ="best") {
+   ## Read Outcome Data
+  
+  ## Check that state and outcome are valid
+  
+  ## Return hospital name in that state with the given rank
+  ## 30- day death rate
+}

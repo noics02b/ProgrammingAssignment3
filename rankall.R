52482a7a8d1bdd7e58058747a30e911c3d76d825
Winston Sumalia <wasumalia@gmail.com>
2014-11-02 22:19:29 +0800

    reorganizing the data frame.1df for all, state hospitalname outcome

--- rankall.R
+++ rankall.R
@@ -6,29 +6,20 @@ rankall <- function(outcome, num="best"){
   
   ## Read Outcome Data
   outcomeOfCare <- read.csv("rprog-data-ProgAssignment3-data/outcome-of-care-measures.csv", colClasses = "character")
-  UniqState <- unique(outcomeOfCare$State)
   UniqOutcome <- c("heart attack","heart failure","pneumonia")
   dimnames(outcomeOfCare)[[2]][[11]]<-"heart.attack"  ##renaming long column name to be referenced later
   dimnames(outcomeOfCare)[[2]][[17]]<-"heart.failure"
   dimnames(outcomeOfCare)[[2]][[23]]<-"pneumonia"
   
   ## Check that state and outcome are valid
-  CheckState<- function(state){
-    for (i in 1:length(UniqState))
-      if (state == UniqState[i] ) { ##print(c("State matched!!", i))
-        return (state)}
-    else if (i == length(UniqState)) return(FALSE)
-  }
   CheckOutcome<- function(outcome){
     for (i in 1:length(UniqOutcome))
       if (outcome == UniqOutcome[i] ) { ##print(c("outcome matched!!", i))
         return (outcome)}
     else if (i == length(UniqOutcome)) return (FALSE) 
   }
-  
-  state<-CheckState(state)
+
   outcome<-CheckOutcome(outcome)
-  if (state == FALSE) stop("invalid state")
   if (outcome == FALSE) stop("invalid outcome")
   
   
@@ -49,9 +40,14 @@ rankall <- function(outcome, num="best"){
   ## Clean data according to outcome only
   cases.ha<-complete.cases(as.numeric(outcomeOfCare[,col.outcome]))
   NoNA<-outcomeOfCare$Provider.Number[cases.ha]
-  newOutcome<-outcomeOfCare[outcomeOfCare$Provider.Number %in% NoNA,]  ## no more NAs on heart.attack!
-  
+  newOutcome<<-outcomeOfCare[outcomeOfCare$Provider.Number %in% NoNA,]  ## no more NAs on heart.attack!
+  #dataout<-data.frame(State= newOutcome[,"State"],HospitalName= newOutcome[,"Hospital.Name"],Outcome=newOutcome[,col.outcome])
+  #dataout<-cbind(c(newOutcome[,"State"],newOutcome[,"Hospital.Name"],newOutcome[,col.outcome]))
+  dataout<-cbind(newOutcome[,"State"],newOutcome[,"Hospital.Name"],newOutcome[,col.outcome])  ## characters
+  dataoutdf<-data.frame(State=dataout[,1],HospitalName= dataout[,2],Outcome=as.numeric(dataout[,3]),stringsAsFactors=FALSE)
   ## For each state, find the hospital of the given rank
+  #ranking <- tapply(newOutcome[,col.outcome],newOutcome$State,function (s) order(newOutcome,newOutcome$Hospital.Name))  ## ranks all ratings for each state.  Output via list
+  return (dataoutdf)
   
   ## Return a data frame with the hospital names and the abbreviated state names
 }
